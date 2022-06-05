1558b87e91827b8657db372bb9e961ca9f5ceb08
szymulcio <szymonwojnarowski@outlook.com>
2022-06-06 01:25:47 +0200

    Finished desktop layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
-const button = document.querySelector(".btn");
-const button2 = document.querySelector(".card");
+const card = document.querySelector(".card");
+const testimonyBtn = document.querySelector(".testimony__btn");
+
+const popup = document.querySelector(".popup");
+const popupBtn = document.querySelector(".popup__btn");
+const overlay = document.querySelector(".overlay");
+
+testimonyBtn.addEventListener("click", function () {
+	popup.classList.add("open");
+	overlay.style.display = "block";
+
+	setTimeout(function () {
+		overlay.classList.add("open");
+	}, 10);
+});
+
+////////////////////
+
+function closePopup() {
+	popup.classList.remove("open");
+	overlay.classList.remove("open");
+	setTimeout(function () {
+		overlay.style.display = "none";
+	}, 200);
+}
+
+overlay.addEventListener("click", closePopup);
+
+popupBtn.addEventListener("click", closePopup);
