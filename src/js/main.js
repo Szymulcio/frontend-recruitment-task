8aaf957ba3cc712e43147d9a277b12afb7cb2858
szymulcio <szymonwojnarowski@outlook.com>
2022-06-06 15:03:02 +0200

    Added click counter

--- src/js/main.js
+++ src/js/main.js
@@ -2,28 +2,61 @@ const card = document.querySelector(".card");
 const testimonyBtn = document.querySelector(".testimony__btn");
 
 const popup = document.querySelector(".popup");
-const popupBtn = document.querySelector(".popup__btn");
+const resetPopupBtn = document.querySelector(".popup__btn");
+const closePopupBtn = document.querySelector(".popup__btn--close");
 const overlay = document.querySelector(".overlay");
 
-testimonyBtn.addEventListener("click", function () {
-	popup.classList.add("open");
+const counter = popup.querySelector("span");
+
+localStorage.timesClicked ||= localStorage.timesClicked = 0;
+
+function openPopup() {
+	popup.style.display = "flex";
 	overlay.style.display = "block";
 
 	setTimeout(function () {
 		overlay.classList.add("open");
-	}, 10);
-});
-
-////////////////////
+		popup.classList.add("open");
+	}, 50);
+}
 
 function closePopup() {
 	popup.classList.remove("open");
 	overlay.classList.remove("open");
+
 	setTimeout(function () {
 		overlay.style.display = "none";
+		popup.style.display = "none";
 	}, 200);
 }
 
-overlay.addEventListener("click", closePopup);
+function handleClick() {
+	updateCounter();
 
-popupBtn.addEventListener("click", closePopup);
+	if (Number(localStorage.timesClicked) >= 5) {
+		resetPopupBtn.style.display = "block";
+		resetPopupBtn.classList.add("open");
+	}
+}
+
+function updateCounter() {
+	localStorage.timesClicked = Number(localStorage.timesClicked) + 1;
+	counter.innerHTML = localStorage.timesClicked;
+}
+
+testimonyBtn.addEventListener("click", function () {
+	openPopup();
+	handleClick();
+});
+
+overlay.addEventListener("click", closePopup);
+closePopupBtn.addEventListener("click", closePopup);
+
+resetPopupBtn.addEventListener("click", function () {
+	localStorage.timesClicked = -1;
+	updateCounter();
+
+	setTimeout(function () {
+		closePopup();
+	}, 2000);
+});
